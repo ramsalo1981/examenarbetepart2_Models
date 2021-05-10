f37a21fd1bcde40011060cabee28af8117a1ddf5
rsalo1981 <55482188+rsalo1981@users.noreply.github.com>
2021-05-10 13:31:38 +0200

    seconf commit

---
+++ ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductRocky_Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
